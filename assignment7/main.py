0383288ca3743b2cace62b05439b7079743f05f0
Computer Education <computereducation@Macmini-213-12.local>
2019-08-08 16:26:39 +0700

    adding assignment7

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,13 +1,17 @@
 from flask import Flask,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 
 @app.route("/")
 def member():
     return render_template('Register.html')
 
-@app.route('/',methods=["post"])
-def index():
-    return 'xyz'
+@app.route('/' ,methods=['post'])
+def Register():
+    return 'Hello Poramajan lattiman'
 
 if __name__ == "__main__":
     app.run(debug=True)
