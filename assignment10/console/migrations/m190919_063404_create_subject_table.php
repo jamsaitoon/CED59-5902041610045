f1728e0b51c20e958a755b4a7735bba1e48cd765
Computer Education <computereducation@Macmini-213-12.local>
2019-09-19 15:28:52 +0700

    Add new assign

--- assignment10/console/migrations/m190919_063404_create_subject_table.php
+++ assignment10/console/migrations/m190919_063404_create_subject_table.php
@@ -14,6 +14,8 @@ class m190919_063404_create_subject_table extends Migration
     {
         $this->createTable('{{%subject}}', [
             'id' => $this->primaryKey(),
+            'name' => $this ->string(256)->notNull(),
+            'detail' => $this->text()
         ]);
     }
 
