2a2f18ad1c4efefc2c800954524fc36ffb06428d
Computer Education <computereducation@Macmini-213-12.local>
2019-09-26 16:32:24 +0700

    update sub

--- assignment10/backend/models/Subject.php
+++ assignment10/backend/models/Subject.php
@@ -1,5 +1,21 @@
 <?php
 namespace backend\models;
 
+use yii\behaviors\TimestampBehavior;
+use yii\behaviors\BlameableBehavior;
+
 class Subject extends \common\models\Subject 
-{}
+{
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::class ,
+            BlameableBehavior::class 
+            
+            
+            
+            
+        ];
+    }
+}
+?>
